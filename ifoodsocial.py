5347d6f37f603ec77bd4c65f7dc684fa07f742c6
Lucas <lubertanhe@gmail.com>
2025-07-31 22:17:02 -0300

    Fix: Gráfico Final

--- ifoodsocial.py
+++ ifoodsocial.py
@@ -2,10 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-arquivo = 'ifood.csv'
-df = pd.read_csv(arquivo, encoding='latin1')
+df = pd.read_csv('ifood.csv', encoding='latin1')
 
-def extrair_estado_civil(row):
+def estado_civil(row):
     if row['marital_Divorced'] == 1:
         return 'Divorciado'
     elif row['marital_Married'] == 1:
@@ -13,19 +12,26 @@ def extrair_estado_civil(row):
     elif row['marital_Single'] == 1:
         return 'Solteiro'
     elif row['marital_Together'] == 1:
-        return 'Juntado'
+        return 'Namorando/Juntos'
     elif row['marital_Widow'] == 1:
         return 'Viúvo'
     else:
         return 'Desconhecido'
 
-df['estado_civil'] = df.apply(extrair_estado_civil, axis=1)
+df['estado_civil'] = df.apply(estado_civil, axis=1)
 
 plt.figure(figsize=(10,6))
-sns.boxplot(x='estado_civil', y='MntTotal', data=df, palette='Set3')
-plt.title('Distribuição dos Gastos Totais por Estado Civil')
+sns.boxplot(x='estado_civil', y='MntTotal', data=df, palette='pastel')
+
+plt.title('Gastos Totais por Estado Civil')
 plt.xlabel('Estado Civil')
-plt.ylabel('Gastos Totais')
-plt.xticks(rotation=45)
+plt.ylabel('Gastos Totais (R$)')
+
+plt.xticks(rotation=30)
+plt.grid(True, axis='y', linestyle='--', alpha=0.7)
+
+formatter = plt.FuncFormatter(lambda x, _: f'R$ {x:,.0f}')
+plt.gca().yaxis.set_major_formatter(formatter)
+
 plt.tight_layout()
 plt.show()
