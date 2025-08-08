c4a8cb7515a11294f58295aec498ced3edd4b13a
Lucas <lubertanhe@gmail.com>
2025-08-07 22:53:26 -0300

    Fix: Nomes

--- analise.estresse.py
+++ analise.estresse.py
@@ -4,6 +4,9 @@ import seaborn as sns
 
 dados = pd.read_csv("Stress_Dataset.csv")
 
+mapa_genero = {0: "Feminino", 1: "Masculino"}
+dados["Gender"] = dados["Gender"].map(mapa_genero)
+
 plt.figure(figsize=(6,4))
 sns.countplot(data=dados, x="Gender", palette="pastel")
 plt.title("Distribuição por Gênero")
