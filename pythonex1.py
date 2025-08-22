82d119762fc01ce4c6769042bdfa82caa9f3e24c
Lucas <lubertanhe@gmail.com>
2025-08-22 19:12:15 -0300

    Feat: 3 Exercicios - Teste aplicado no N8 (OFf)

---
+++ pythonex1.py
@@ -0,0 +1,20 @@
+# Exercicios Python (Parte 1) - Realizar para cada dataset e algoritmos diferentes, criar um modelo de machine learning e apresentar a árvore de decisão.
+#1 – Leitura e Visualização Inicial
+import pandas as pd
+notas = pd.read_csv('notas.csv')
+print("Entendendo o Dataset:")
+print(notas.head())
+print("\nInformações do dataset:")
+print(notas.info())
+print("\nEstatísticas descritivas:")
+print(notas.describe())
+#2 – Limpeza de Dados
+#3 – Filtragem e Seleção
+#4 – Criação de Novas Colunas
+#5 – Agrupamento de Dados
+#6 – Análise Estatística Simples
+#7 – Visualização de Dados
+#8 – Detecção de Outliers
+#9 – Correlação entre Variáveis
+#10 – Exportação de Dados Tratados
+#Utilizar o notas.csv, e depois apoiar o machine learning com o dataset Stress_Dataset.csv 
